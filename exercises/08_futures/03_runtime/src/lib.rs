98c197ce150e214dcc9c8317e765a97ca96d4dda
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-10-14 14:16:51 +0800

    feat: 08 futures (00 intro - 03 runtime) finished

--- exercises/08_futures/03_runtime/src/lib.rs
+++ exercises/08_futures/03_runtime/src/lib.rs
@@ -1,4 +1,4 @@
-// TODO: Implement the `fixed_reply` function. It should accept two `TcpListener` instances,
+//  Implement the `fixed_reply` function. It should accept two `TcpListener` instances,
 //  accept connections on both of them concurrently, and always reply to clients by sending
 //  the `Display` representation of the `reply` argument as a response.
 use std::fmt::Display;
